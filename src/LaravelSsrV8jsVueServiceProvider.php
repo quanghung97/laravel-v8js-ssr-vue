eca67d296d13fabd2e9f6d5822618e5d3e6cfe17
quanghung97 <tran.quang.hungb@sun-asterisk.com>
2019-05-30 21:55:51 +0700

    first commit

---
+++ src/LaravelSsrV8jsVueServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Quanghung97\laravelV8jsSsrVue;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSsrV8jsVueServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/V8JsVueController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class V8JsVueController extends Controller
+{
+    public function render()
+    {
+        // Get the vue server renderer script
+        $renderer_source = \File::get(base_path('node_modules/vue-server-renderer/basic.js'));
+
+        // Get the server entry point
+        $app_source = \File::get(public_path('js/server.js'));
+
+        // define some local variable to our environment
+
+        $js = '
+            var process = { env: { VUE_ENV: "server", NODE_ENV: "production" } };
+            this.global = { process: process };
+        ';
+
+        $v8 = new \V8js();
+
+        ob_start();
+
+        $v8->executeString($js);
+        $v8->executeString($renderer_source);
+        $v8->executeString($app_source);
+
+        return ob_get_clean();
+    }
+}
